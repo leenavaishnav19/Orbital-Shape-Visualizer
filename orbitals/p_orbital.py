9ee0a8693d625d2dec10071290b406d1c9530cb6
Leena Vaishnav <leena.25bce11206@vitbhopal.ac.in>
2026-04-13 10:03:11 +0530

    Update p_orbital.py

--- orbitals/p_orbital.py
+++ orbitals/p_orbital.py
@@ -1,1 +1,14 @@
+import numpy as np
 
+def generate_p_orbital():
+    theta = np.linspace(0, 2*np.pi, 100)
+    phi = np.linspace(0, np.pi, 100)
+    theta, phi = np.meshgrid(theta, phi)
+
+    r = np.cos(phi)
+
+    x = r * np.sin(phi) * np.cos(theta)
+    y = r * np.sin(phi) * np.sin(theta)
+    z = r * np.cos(phi)
+
+    return x, y, z
